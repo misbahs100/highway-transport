3c7356fe4e0de4a5f069bb775b0989d946678585
misbahs100 <misbahuddins100@gmail.com>
2021-03-20 01:02:23 +0600

    date/calendar at address page, showing source and destination after search

--- src/components/DestinationAddress/DestinationAddress.js
+++ src/components/DestinationAddress/DestinationAddress.js
@@ -3,11 +3,15 @@ import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 import './DestinationAddress.css';
 import {UserContext} from '../../App'
-import Header from '../Header/Header';
+import Header from '../Header/Header'; 
 import Map from '../Map/Map';
 import transportsData from '../../transportsData/transportsData.json';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import Datetime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+import moment from 'moment';
+import 'moment/locale/fr';
 
 const DestinationAddress = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -17,6 +21,20 @@ const DestinationAddress = () => {
     const { vehicle } = useParams();
     const { register, handleSubmit, watch, errors } = useForm();
     const [isSearched, setIsSearched] = useState(false);
+    const [address, setAddress] = useState({
+        source: '',
+        destination: ''
+    })
+    const handleOnBlur = (e) => {
+       
+    //    if(e.target.name === 'source'){
+           const newAddress = {...address};
+           newAddress[e.target.name] = e.target.value;
+           console.log(e.target.value);
+           setAddress(newAddress);
+           console.log("bilai",address)
+    //    }
+    }
     const onSubmit = data => {
         console.log(data);
         setIsSearched(true);
@@ -29,19 +47,22 @@ const DestinationAddress = () => {
             <Header></Header>
             <div className="row container mt-5">
                 {isSearched
-                  ?  ( <div className="col-md-4 container" style={{border:'1px solid black', padding:'10px'}}>
-                  <div className="timeline"></div>
-                  <div className="d-flex detail">
+                  ?  ( <div className="col-md-4 container" style={{border:'1px solid black', padding:'15px', backgroundColor: 'rgb(40, 40, 48)'}}>
+                  <div className="timeline">
+                      <div className="timeline-text">Source: {address.source}</div>
+                      <div className="timeline-text">Destination: {address.destination}</div>
+                  </div>
+                  <div className="d-flex detail bg-white">
                       <div className="img-address"><img src={result.imageUrl} alt=""/></div>
                       <div><p>{result.title} <FontAwesomeIcon icon={faUserFriends} /> {result.capacity}</p></div>
                       <div><p>{result.price}</p></div>
                   </div>
-                  <div className="d-flex detail">
+                  <div className="d-flex detail bg-white">
                       <div className="img-address"><img src={result.imageUrl} alt=""/></div>
                       <div><p>{result.title} <FontAwesomeIcon icon={faUserFriends} /> {result.capacity}</p></div>
                       <div><p>{result.price}</p></div>
                   </div>
-                  <div className="d-flex detail">
+                  <div className="d-flex detail bg-white">
                       <div className="img-address"><img src={result.imageUrl} alt=""/></div>
                       <div><p>{result.title} <FontAwesomeIcon icon={faUserFriends} /> {result.capacity}</p></div>
                       <div><p>{result.price}</p></div>
@@ -49,6 +70,7 @@ const DestinationAddress = () => {
               </div>)
                 : ( <div className="col-md-4 container addressbox">
                 <h3 className="text-center">Giv.....</h3>
+                
                 <form className="address-form" onSubmit={handleSubmit(onSubmit)}>
                     
                     <input name="name" defaultValue={loggedInUser.displayName} ref={register({ required: true })} placeholder="Your name"/>
@@ -57,11 +79,14 @@ const DestinationAddress = () => {
                     <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your email" />
                     {errors.email && <span className="error">Email is required</span>}
 
-                    <input name="source" ref={register({ required: true })} placeholder="Pick from" />
+                    <input name="source" onBlur={handleOnBlur} ref={register({ required: true })} placeholder="Pick from" />
                     {errors.source && <span className="error">Source is required</span>}
 
-                    <input name="destination" ref={register({ required: true })} placeholder="Destination" />
+                    <input name="destination" onBlur={handleOnBlur} ref={register({ required: true })} placeholder="Destination" />
                     {errors.destination && <span className="error">Destination is required</span>}
+
+                    <p className="date-p">Click below to set date.</p>
+                    <Datetime className="calendar" locale="en-ca"/>
 
                     <input type="submit" value="Search"/>
                 </form>
@@ -72,6 +97,7 @@ const DestinationAddress = () => {
                
                 <div className="col-md-6" style={{border: '1px solid red'}}>
                     <Map></Map>
+                    
                 </div>
             </div>
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,23 +10,23 @@ const Header = () => {
             
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid">
-                    <Link to="/home" class="navbar-brand header-text">Highway Transport</Link>
+                    <Link to="/home" class="navbar-brand header-text"><p>Highway Transport</p></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <Link to="/home"  className="nav-link active mr-4">Home</Link>
+                                <Link to="/home"  className="nav-link active mr-4 header-text"><p>Home</p></Link>
                             </li>
                             <li class="nav-item">
-                            <Link to="/blog" className="nav-link mr-4">Blog</Link>
+                            <Link to="/blog" className="nav-link mr-4 header-text"><p>Blog</p></Link>
                             </li>
                             <li class="nav-item">
-                            <Link to="/contact"  className="nav-link mr-4">Contact</Link>
+                            <Link to="/contact"  className="nav-link mr-4 header-text"><p>Contact</p></Link>
                             </li>
                             <li class="nav-item">
-                            <Link to="/login"  className="nav-link active mr-4 ml-2 header-text">{loggedInUser.displayName ? loggedInUser.displayName : "Login"}</Link>
+                            <Link to="/login"  className="nav-link active mr-4 ml-2 header-text">{loggedInUser.displayName ? <p>{loggedInUser.displayName}</p>  : <p>Login</p>}</Link>
                             </li>
                         </ul>
                     </div>
